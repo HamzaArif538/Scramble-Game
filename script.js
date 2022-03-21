c3c666c16afb7bd48314b9396bc42bd40a8e46dd
HamzaArif538 <sardarhamza538@gmail.com>
2022-03-21 20:52:57 +0500

    completed the game

--- script.js
+++ script.js
@@ -2,18 +2,54 @@ const msg = document.querySelector('.msg');
 const guess = document.querySelector('input');
 const btn = document.querySelector('.btn');
 let play = false;
-let countries = ["Afghanistan","Pakistan","India","Palestine","Russia","Iraq","America","Germany","France","Albania","Algeria","Andorra","Angola","Anguilla","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi"]
+let newWords = "";
+let randWords = "";
+let countries = ["Afghanistan","Austria","Netherland", "Pakistan", "India", "Palestine", "Russia", "Iraq", "America", "Germany", "France", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi"]
 
 const createNewWords = () => {
-    let ranNum = (Math.random() * countries.length);
-    console.log(ranNum);
+    let ranNum = Math.floor(Math.random() * countries.length);
+    // console.log(ranNum);
+    let newTempCountries = countries[ranNum];
+    // console.log(newTempCountries.split(""));
+    return newTempCountries;
 }
 
-btn.addEventListener('click', function() {
+const scrambleWords = (arr) => {
+    for (let i = arr.length -1 ; i>0; i--){
+        let temp = arr[i];
+        // console.log(temp);
+        let j = Math.floor(Math.random() * (i+1));
+        // console.log(i);
+        // console.log(j);
+
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
+}
+
+btn.addEventListener('click', function () {
     if (!play) {
         play = true;
         btn.innerHTML = "Guess"
         guess.classList.toggle('hidden')
-        createNewWords()
+        newWords = createNewWords();
+        randWords = scrambleWords(newWords.split("")).join("");
+        msg.innerHTML = `Guess the Word: ${randWords}`;
+    }
+    else{
+        let tempWord = guess.value;
+        if(tempWord === newWords){
+            // console.log("Correct");
+            play = false
+            msg.innerHTML = `Congratulations It's Correct, It is ${newWords}`;
+            btn.innerHTML = "Start Again";
+            guess.classList.toggle("hidden");
+            guess.value = "";
+        }
+        else{
+            console.log("Incorrect");
+            msg.innerHTML = `Sorry It's Not Correct. Please Try Again ${randWords}`;
+        }
     }
 })
